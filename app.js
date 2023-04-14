a8c55a0062927f4eeadfda82ee6d6dca28987948
hurayChan <chan@huray.net>
2023-04-14 09:03:19 +0000

    [UPDATE] fix get signature

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const makeSignature = require('./modules/signature/signature.js')
+require('dotenv').config();
+const apigw = process.env['NCR_API_GATEWAY_URL'];
+const fetch = require('request-promise-native');
+const getHeader = require('./modules/signature/signature.js');
+const timeout = process.env['TIMEOUT'] || 5000;
 
-console.log(makeSignature('GET','/'));
+console.log(`apigw: ${apigw}`)
+let method = 'GET';
+let path = `/ncr/api/v2/repositories`;
+let headers = getHeader(method,path);
+headers['Content-Type'] = 'application/json; charset=utf-8';
+console.log(headers);
+
+fetch({
+  uri: `${apigw}${path}`,
+  method,
+  headers,
+  timeout
+})
+.then(res=>{
+  console.log(res);
+  console.log("arstasrteiranstasjrtie")
+})
+.catch(err=>{
+  // console.error(err);
+})
--- modules/signature/signature.js
+++ modules/signature/signature.js
@@ -1,27 +1,26 @@
 require('dotenv').config();
-require('../components/enc-base64-min.js');
+require('../components/enc-base64-min.js')();
 const CryptoJS = require('../rollups/hmac-sha256.js');
 
-let makeSignature = (method, url)=>{
-	let space = ' ';
-	let newLine = '\n';
-	// let method = "GET";
-	// let url = "/photos/puppy.jpg?query1=&query2";
-	let timestamp = '1681439143336';			// current timestamp (epoch)
-	let accessKey = process.env['NCR_ACCESS_KEY'];
+let accessKey = process.env['NCR_ACCESS_KEY'];
+
+let makeSignature = (method, path, timestamp)=>{
 	let secretKey = process.env['NCR_SECRET_KEY'];
-
 	let hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
-	hmac.update(method);
-	hmac.update(space);
-	hmac.update(url);
-	hmac.update(newLine);
-	hmac.update(timestamp);
-	hmac.update(newLine);
-	hmac.update(accessKey);
-
+  let sig = `${method} ${path}\n${timestamp}\n${accessKey}`;
+  hmac.update(sig);
 	let hash = hmac.finalize();
-
 	return hash.toString(CryptoJS.enc.Base64);
 }
-module.exports = makeSignature;
+
+let getHeader = (method,path)=>{
+  let timestamp = String(new Date().valueOf());
+  let sig = makeSignature(method,path,timestamp);
+  return {
+    'x-ncp-apigw-timestamp':timestamp,
+    'x-ncp-iam-access-key':accessKey,
+    'x-ncp-apigw-signature-v2':sig
+  }
+}
+
+module.exports = getHeader;
