87956a6d7f522c6541f7a8f1698871b52e53aff3
hurayChan <chan@huray.net>
2023-04-14 14:40:02 +0000

    [UPDATE] v1 stash

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
-const apigw = process.env['NCR_API_GATEWAY_URL'];
+const apigw = process.env['NCR_API_GATEWAY_URL'] || 'https://ncr.apigw.ntruss.com';
 const fetch = require('request-promise-native');
 const getHeader = require('./modules/signature/signature.js');
 const timeout = process.env['TIMEOUT'] || 5000;
 
-console.log(`apigw: ${apigw}`)
 let method = 'GET';
-let path = `/ncr/api/v2/repositories`;
+let path = '/ncr/api/v2/repositories';
 let headers = getHeader(method,path);
 headers['Content-Type'] = 'application/json; charset=utf-8';
-console.log(headers);
 
 fetch({
   uri: `${apigw}${path}`,
@@ -18,9 +16,8 @@ fetch({
   timeout
 })
 .then(res=>{
-  console.log(res);
-  console.log("arstasrteiranstasjrtie")
+  console.log(JSON.parse(res));
 })
 .catch(err=>{
-  // console.error(err);
+  console.error(err);
 })
