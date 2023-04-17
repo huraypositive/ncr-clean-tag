aa8ba55ce412220257c37cda8165f3604b03c826
hurayChan <chan@huray.net>
2023-04-17 12:01:09 +0000

    [UPDATE] get done

--- v2/main.go
+++ v2/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	"nct/router"
-	"os"
+  "fmt"
+  "nct/router"
+  "os"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		os.Stderr.WriteString("arg required\n")
-		os.Exit(2)
-	}
+var command string
+var version string
 
-	switch os.Args[1] {
-	case "get":
-		router.Get()
-	}
-	// if os.Args[1] == "get" {
-	//   router.Get()
-	// }
+func init() {
+  if command == "" {
+    command = "nct"
+  }
+}
+
+func main() {
+  // version
+  if os.Args[1] == "version" || os.Args[1] == "--version" || os.Args[1] == "-v" {
+    fmt.Printf("%s version: %s\n", command, version)
+    return
+  }
+
+  if len(os.Args) < 3 {
+    os.Stderr.WriteString("arg required\n")
+    os.Exit(2)
+  }
+
+  switch os.Args[1] {
+  case "get":
+    router.Get()
+  }
+  // if os.Args[1] == "get" {
+  //   router.Get()
+  // }
 }
--- v2/router/get.go
+++ v2/router/get.go
@@ -1,174 +1,235 @@
 package router
 
 import (
-	"encoding/json"
-	"fmt"
+  "encoding/json"
+  "fmt"
   "gopkg.in/yaml.v3"
-	"nct/config"
-	"nct/signature"
+  "nct/config"
+  "nct/signature"
   "net/url"
-	"os"
-	"strconv"
+  "os"
+  "strconv"
 )
 
 type Results []interface{}
 type Body struct {
-	Next    int                 `json:"next"`
-	Results Results             `json:"results"`
+  Next    int     `json:"next"`
+  Results Results `json:"results"`
 }
 
 func Get() {
-	apiSpec := ApiSpec{}
-	switch os.Args[2] {
-	case "registry":
-		flagConfig := getFlagConfig("registry")
-		apiSpec.getRepos(flagConfig)
-	case "image":
-		fallthrough
-	case "images":
-		if len(os.Args) < 4 || (len(os.Args) >= 4 && string([]rune(os.Args[3])[0]) == "-") {
-			flagConfig := getFlagConfig("image")
-			apiSpec.getImages(flagConfig)
-		} else {
-			flagConfig := getFlagConfig("image")
-			apiSpec.getImageDetail(flagConfig)
-		}
-	case "tag":
-		fallthrough
-	case "tags":
-		if len(os.Args) < 4 || (len(os.Args) >= 4 && string([]rune(os.Args[3])[0]) == "-") {
-			flagConfig := getFlagConfig("tag")
-			apiSpec.getTags(flagConfig)
-		} else {
-			flagConfig := getFlagConfig("tag")
-			apiSpec.getTagDetail(flagConfig)
-		}
-	}
+  apiSpec := ApiSpec{}
+  switch os.Args[2] {
+  case "registry":
+    flagConfig := getFlagConfig("registry")
+    apiSpec.getRegistry(flagConfig)
+  case "image":
+    fallthrough
+  case "images":
+    if len(os.Args) < 4 || (len(os.Args) >= 4 && string([]rune(os.Args[3])[0]) == "-") {
+      flagConfig := getFlagConfig("image")
+      apiSpec.getImages(flagConfig)
+    } else {
+      flagConfig := getFlagConfig("image")
+      apiSpec.getImageDetail(flagConfig)
+    }
+  case "tag":
+    fallthrough
+  case "tags":
+    if len(os.Args) < 4 || (len(os.Args) >= 4 && string([]rune(os.Args[3])[0]) == "-") {
+      flagConfig := getFlagConfig("tag")
+      apiSpec.getTags(flagConfig)
+    } else {
+      flagConfig := getFlagConfig("tag")
+      apiSpec.getTagDetail(flagConfig)
+    }
+  }
 }
 
-func (apiSpec *ApiSpec) getRepos(flagConfig *config.Flag) {
-	apiSpec.method = "GET"
-	apiSpec.path = "/ncr/api/v2/repositories"
-	apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
-	apiSpec.jsonContent = true
-	data, err := sendRequest(apiSpec)
-	if err != nil {
-		return
-	}
-	if flagConfig.Output == "json" {
-		fmt.Println(string(*data))
-		return
-	}
-	var body Body
-	err = json.Unmarshal(*data, &body)
+func (apiSpec *ApiSpec) getRegistry(flagConfig *config.Flag) {
+  apiSpec.method = "GET"
+  apiSpec.path = "/ncr/api/v2/repositories"
+  apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
+  apiSpec.jsonContent = true
+  data, err := sendRequest(apiSpec)
   if err != nil {
     return
   }
-	if flagConfig.Output == "yaml" {
-		yamlString, err := yaml.Marshal(body)
-		if err != nil {
-			return
-		}
-		fmt.Println(string(yamlString))
-		return
-	}
-	if !flagConfig.NoHeaders {
-		fmt.Println("NAME")
-	}
-	for _, v := range body.Results {
-		fmt.Println(v.(map[string]interface{})["name"])
-	}
+  if flagConfig.Output == "json" {
+    fmt.Println(string(*data))
+    return
+  }
+  var body Body
+  err = json.Unmarshal(*data, &body)
+  if err != nil {
+    return
+  }
+  if flagConfig.Output == "yaml" {
+    yamlString, err := yaml.Marshal(body)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(yamlString))
+    return
+  }
+  if !flagConfig.NoHeaders {
+    fmt.Println("NAME")
+  }
+  for _, v := range body.Results {
+    fmt.Println(v.(map[string]interface{})["name"])
+  }
 }
 
 func (apiSpec *ApiSpec) getImages(flagConfig *config.Flag) {
-	var results Results
-	for i := 1; ; i++ {
-		apiSpec.method = "GET"
-		apiSpec.path = "/ncr/api/v2/repositories/" + flagConfig.Registry + "?page=" + strconv.Itoa(i)
-		apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
-		apiSpec.jsonContent = true
-		data, err := sendRequest(apiSpec)
-		if err != nil {
-			return
-		}
-		var body Body
-		err = json.Unmarshal(*data, &body)
-		for _, v := range body.Results {
-			results = append(results, v)
-		}
-		if body.Next == 0 {
-			break
-		}
-	}
-	if flagConfig.Output == "json" {
-		jsonString, err := json.Marshal(results)
-		if err != nil {
-			return
-		}
-		fmt.Println(string(jsonString))
-		return
-	}
-	if flagConfig.Output == "yaml" {
-		yamlString, err := yaml.Marshal(results)
-		if err != nil {
-			return
-		}
-		fmt.Println(string(yamlString))
-		return
-	}
-	if !flagConfig.NoHeaders {
-		fmt.Println("NAME")
-	}
-	for _, v := range results {
-		fmt.Println(v.(map[string]interface{})["name"])
-	}
+  var results Results
+  for i := 1; ; i++ {
+    apiSpec.method = "GET"
+    apiSpec.path = "/ncr/api/v2/repositories/" + flagConfig.Registry + "?page=" + strconv.Itoa(i)
+    apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
+    apiSpec.jsonContent = true
+    data, err := sendRequest(apiSpec)
+    if err != nil {
+      return
+    }
+    var body Body
+    err = json.Unmarshal(*data, &body)
+    for _, v := range body.Results {
+      results = append(results, v)
+    }
+    if body.Next == 0 {
+      break
+    }
+  }
+  if flagConfig.Output == "json" {
+    jsonString, err := json.Marshal(results)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(jsonString))
+    return
+  }
+  if flagConfig.Output == "yaml" {
+    yamlString, err := yaml.Marshal(results)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(yamlString))
+    return
+  }
+  if !flagConfig.NoHeaders {
+    fmt.Println("NAME")
+  }
+  for _, v := range results {
+    fmt.Println(v.(map[string]interface{})["name"])
+  }
 }
 
 func (apiSpec *ApiSpec) getImageDetail(flagConfig *config.Flag) {
+  apiSpec.method = "GET"
+  apiSpec.path = "/ncr/api/v2/repositories/" + flagConfig.Registry + "/" + url.QueryEscape(os.Args[3])
+  apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
+  apiSpec.jsonContent = true
+  data, err := sendRequest(apiSpec)
+  if err != nil {
+    return
+  }
+  if flagConfig.Output == "json" {
+    fmt.Println(string(*data))
+    return
+  }
+  body := make(map[string]interface{})
+  err = json.Unmarshal(*data, &body)
+  if err != nil {
+    return
+  }
+  if flagConfig.Output == "yaml" {
+    yamlString, err := yaml.Marshal(body)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(yamlString))
+    return
+  }
+  if !flagConfig.NoHeaders {
+    fmt.Println("NAME")
+  }
+  fmt.Println(body["name"])
 }
+
 func (apiSpec *ApiSpec) getTags(flagConfig *config.Flag) {
-	var results Results
-	for i := 1; ; i++ {
-		apiSpec.method = "GET"
-		apiSpec.path = "/ncr/api/v2/repositories/" + flagConfig.Registry + "/" +
-    url.QueryEscape(flagConfig.Image) + "/tags?page=" + strconv.Itoa(i)
-		apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
-		apiSpec.jsonContent = true
-		data, err := sendRequest(apiSpec)
-		if err != nil {
-			return
-		}
-		var body Body
-		err = json.Unmarshal(*data, &body)
-		for _, v := range body.Results {
-			results = append(results, v)
-		}
-		if body.Next == 0 {
-			break
-		}
-	}
-	if flagConfig.Output == "json" {
-		jsonString, err := json.Marshal(results)
-		if err != nil {
-			return
-		}
-		fmt.Println(string(jsonString))
-		return
-	}
-	if flagConfig.Output == "yaml" {
-		yamlString, err := yaml.Marshal(results)
-		if err != nil {
-			return
-		}
-		fmt.Println(string(yamlString))
-		return
-	}
-	if !flagConfig.NoHeaders {
-		fmt.Println("NAME")
-	}
-	for _, v := range results {
-		fmt.Println(v.(map[string]interface{})["name"])
-	}
+  var results Results
+  for i := 1; ; i++ {
+    apiSpec.method = "GET"
+    apiSpec.path = "/ncr/api/v2/repositories/" + flagConfig.Registry + "/" +
+      url.QueryEscape(flagConfig.Image) + "/tags?page=" + strconv.Itoa(i)
+    apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
+    apiSpec.jsonContent = true
+    data, err := sendRequest(apiSpec)
+    if err != nil {
+      return
+    }
+    var body Body
+    err = json.Unmarshal(*data, &body)
+    for _, v := range body.Results {
+      results = append(results, v)
+    }
+    if body.Next == 0 {
+      break
+    }
+  }
+  if flagConfig.Output == "json" {
+    jsonString, err := json.Marshal(results)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(jsonString))
+    return
+  }
+  if flagConfig.Output == "yaml" {
+    yamlString, err := yaml.Marshal(results)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(yamlString))
+    return
+  }
+  if !flagConfig.NoHeaders {
+    fmt.Println("NAME")
+  }
+  for _, v := range results {
+    fmt.Println(v.(map[string]interface{})["name"])
+  }
 }
+
 func (apiSpec *ApiSpec) getTagDetail(flagConfig *config.Flag) {
+  apiSpec.method = "GET"
+  apiSpec.path = "/ncr/api/v2/repositories/" + flagConfig.Registry + "/" +
+    url.QueryEscape(flagConfig.Image) + "/tags/" + os.Args[3]
+  apiSpec.headers = signature.GetHeader(&apiSpec.method, &apiSpec.path)
+  apiSpec.jsonContent = true
+  data, err := sendRequest(apiSpec)
+  if err != nil {
+    return
+  }
+  if flagConfig.Output == "json" {
+    fmt.Println(string(*data))
+    return
+  }
+  body := make(map[string]interface{})
+  err = json.Unmarshal(*data, &body)
+  if err != nil {
+    return
+  }
+  if flagConfig.Output == "yaml" {
+    yamlString, err := yaml.Marshal(body)
+    if err != nil {
+      return
+    }
+    fmt.Println(string(yamlString))
+    return
+  }
+  if !flagConfig.NoHeaders {
+    fmt.Println("NAME")
+  }
+  fmt.Println(body["name"])
 }
